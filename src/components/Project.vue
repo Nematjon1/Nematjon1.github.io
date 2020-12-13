9a680fc09c4f44a9a5916eab2ccc639c56e9ce8d
Nematjon Abdulloev <frank.sport.93@gmail.com>
2020-12-13 10:16:55 +0300

    Projects page draft

---
+++ src/components/Project.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="project">
+    <a v-bind:href="project.link" class="project__link">{{ project.name }}</a>
+    <p class="project__description">{{ project.description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Project",
+
+  props: {
+    project: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.project {
+  max-width: 600px;
+  width: 100%;
+  box-shadow: 2px 5px 10px rgba(0, 0, 0, 0.3);
+  background-color: white;
+  margin: 1rem auto;
+  text-indent: 8px;
+  border: 2px solid hsl(200, 60%, 70%);
+  border-radius: 0.4rem;
+  padding: 0.7rem 1rem;
+  line-height: 1.4;
+
+  .project__link:link,
+  .project__link:visited {
+    font-size: 1.2rem;
+    color: hsl(200, 40%, 40%);
+    text-decoration: none;
+  }
+
+  .project__link:hover,
+  .project__link:active {
+    color: hsl(200, 40%, 30%);
+    cursor: pointer;
+  }
+}
+</style>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,13 +1,56 @@
 <template>
   <div>
     <h1>Projects Page</h1>
+
+    <div class="pt-4 projects">
+      <Project
+        v-for="project of projects"
+        v-bind:project="project"
+        v-bind:key="project.id"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import Project from "@/components/Project";
+
 export default {
-  name: "Projects"
+  name: "Projects",
+  components: {
+    Project
+  },
+
+  data() {
+    return {
+      projects: [
+        {
+          id: 1,
+          name: "App",
+          description: "Vue Application",
+          link: "https://github.com/Nematjon1"
+        },
+        {
+          id: 2,
+          name: "App",
+          description: "Vue Application",
+          link: "https://github.com/Nematjon1"
+        },
+        {
+          id: 3,
+          name: "App",
+          description: "Vue Application",
+          link: "https://github.com/Nematjon1"
+        }
+      ]
+    };
+  }
 };
 </script>
 
-<style></style>
+<style>
+.projects {
+  display: flex;
+  flex-flow: wrap;
+}
+</style>
